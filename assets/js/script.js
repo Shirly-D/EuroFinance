f6b044512e54bfa6822570ae20c6265f83513c43
Shirly <shirlyd@axioned.com>
2021-08-23 22:34:44 +0530

    js all functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var position = document.querySelector('.position');
 var company = document.querySelector('.company');
 var compType = document.querySelector('.comp-type');
 var country = document.querySelector('.country');
-var workEmail = document.querySelector('work-email');
+var workEmail = document.querySelector('.work-email');
 var checkYes = document.querySelector('.yes');
 var checkNo = document.querySelector('.no');
 var nameLength = /^[A-Za-z. ]{3,30}$/;
@@ -21,7 +21,48 @@ function inputValue() {
     } else {
         successMsg(fname);
     }
-    
+    if(!nameLength.test(lname.value)) {
+        errorMsg(lname, '*This field is required');
+    } else {
+        successMsg(lname);
+    }
+    if(!nameLength.test(position.value)) {
+        errorMsg(position, '*This field is required');
+    } else {
+        successMsg(position);
+    }
+    if(!nameLength.test(company.value)) {
+        errorMsg(company, '*This field is required');
+    } else {
+        successMsg(company);
+    }
+    if(!nameLength.test(compType.value)) {
+        errorMsg(compType, '*This field is required');
+    } else {
+        successMsg(compType);
+    }
+    if(!nameLength.test(country.value)) {
+        errorMsg(country, '*This field is required');
+    } else {
+        successMsg(country);
+    }
+    if(!nameLength.test(workEmail.value)) {
+        errorMsg(workEmail, '*This field is required');
+    } else if (!isEmail(email)) {
+        errorMsg(workEmail, 'Email is invalid');
+    } else {
+        successMsg(workEmail);
+    }  
+    if(!checkYes == checked) {
+        errorMsg(checkYes, '*This field is required');
+    } else {
+        successMsg(checkYes);
+    }
+    if(checkNo) {
+        errorMsg(checkNo, '*This field is required');
+    } else {
+        successMsg(checkNo);
+    }
 }
 
 function errorMsg(input, message) {
@@ -35,7 +76,6 @@ function successMsg(input) {
     var formControl = input.parentElement;
     formControl.className = ' form-control success';
 }
-
 
 function isEmail(email) {
     return /^[A-Za-z_]{3,}[0-9]{2,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/.test(email);
