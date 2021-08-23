f6b0444e0f858bae5c6cb915b212999657ed0f9b
Shirly <shirlyd@axioned.com>
2021-08-23 17:22:15 +0530

    js initial commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
-/* Author: 
-
-*/
+var firstName = document.querySelector('.first');
+var lastName = document.querySelector('.last');
+var position = document.querySelector('.position');
+var company = document.querySelector('.company');
+var compType = document.querySelector('.comp-type');
+var country = document.querySelector('.country');
+var workEmail = document.querySelector('work-email');
+var checkYes = document.querySelector('.yes');
+var checkNo = document.querySelector('.no');
 
 
 
